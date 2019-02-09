c3fa2efac13da2f6cd26cecf4d9152627cdfdae4
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-09 12:19:48 +0200

    09.02.2019

--- 2019.02.09/11. SpeedConvertion/Program.cs
+++ 2019.02.09/11. SpeedConvertion/Program.cs
@@ -11,7 +11,19 @@ namespace _11.SpeedConvertion
         // 11. Преобразуване на скорост
         static void Main(string[] args)
         {
-
+            // Входни данни: метри, часове, минути, секунди
+            double meters = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double m = double.Parse(Console.ReadLine());
+            double s = double.Parse(Console.ReadLine());
+            // сметки
+            double time = h * 3600.0 + m * 60.0 + s;
+            // скоростта в метри в секунди (m/s)
+            Console.WriteLine(Math.Round(meters / time,6));
+            // скоростта в километри в час(km / h)
+            Console.WriteLine(Math.Round((meters / 1000.0) / (time/3600.0),6));
+            // скоростта в мили в час(mp / h)
+            Console.WriteLine(Math.Round((meters / 1609.0) / (time/3600.0),6));
         }
     }
 }

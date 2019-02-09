eac526c0a57b74c29db2ead8a56ace9a38ba8ab6
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-09 11:49:57 +0200

    09.02.2019

---
+++ 2019.02.09/11. SpeedConvertion/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.SpeedConvertion
+{
+    class Program
+    {
+        // 11. Преобразуване на скорост
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
